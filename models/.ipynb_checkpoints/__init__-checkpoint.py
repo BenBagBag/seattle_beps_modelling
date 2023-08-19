f15e27bbcc7317afafd76f7fcc9bb101aff2f66e
BenBagBag <isaac.cowhey@gmail.com>
2023-08-18 19:37:10 -0700

    add original building data

--- models/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,1 +0,0 @@
-__all__ = ['baseline_model', 'alternate_proposal_model']
--- models_new/baseline_model.py
+++ models_new/baseline_model.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class BaselineBEPSModel:
     # be sure to remove scenario results option
-    def __init__(self, emissions_path, timeline_path, building_data_path, fine_years, fine_per_sqft, scenario_results = None):
+    def __init__(self, emissions_path, timeline_path, building_data_path, fine_years, fine_per_sqft):
         '''
         emissions_path: file path to table of energy emissions factors for each year
         timeline_path: file path for proposed timeline of emissions reduction
@@ -15,7 +15,6 @@ class BaselineBEPSModel:
         self.building_data_path = building_data_path
         self.fine_years = fine_years
         self.fine_per_sqft = fine_per_sqft
-        self.scenario_results = pd.read_csv(scenario_results)
 
     # Loading data
     
@@ -37,18 +36,15 @@ class BaselineBEPSModel:
 
     # Calculating the baseline model
     
-    def _find_ghgi_standard(self, year, building_type, sq_ft_class, alt_timeline = None):
+    def _find_ghgi_standard(self, year, building_type, sq_ft_class):
         '''
         Find the GHGI standard in the model's timeline for a given year, building type, and building size.
-
-        alt_timeline: timeline data frame
         '''
         # building types listed as NAN don't count towards the policy, so their emissions and sq ft are calculated as zero
         if pd.isna(building_type) or building_type == 'nan':
             return 0
 
-        timeline = alt_timeline or self.timeline
-        row = timeline[(timeline['year'] == year) & (timeline['sq_ft_classification'] == sq_ft_class) & (timeline['building_type'] == building_type)]
+        row = self.timeline[(self.timeline['year'] == year) & (self.timeline['sq_ft_classification'] == sq_ft_class) & (self.timeline['building_type'] == building_type)]
         return row.iloc[0]['ghgi'] 
     
     def _get_expected_baseline(self, building, year):
@@ -84,17 +80,14 @@ class BaselineBEPSModel:
 
         return output_df
     
-    def _get_city_ghgi(self, building, alt_timeline = None):
-        '''
-        alt_timeline: dataframe of alternative target timeline
-        '''
+    def _get_city_ghgi(self, building):
         year = building['year']
         baseline_ghgi = building['expected_baseline_ghgi']
         orig_building_row = self.building_data[self.building_data['OSEBuildingID'] == building['OSEBuildingID']].iloc[0]
 
-        first_use_ghgi = self._find_ghgi_standard(year, orig_building_row['LargestPropertyUseType OSE'], building['sq_ft_classification'], alt_timeline)
-        second_use_ghgi = self._find_ghgi_standard(year, orig_building_row['SecondLargestPropertyUseType OSE'], building['sq_ft_classification'], alt_timeline)
-        third_use_ghgi = self._find_ghgi_standard(year, orig_building_row['ThirdLargestPropertyUseType OSE'], building['sq_ft_classification'], alt_timeline)
+        first_use_ghgi = self._find_ghgi_standard(year, orig_building_row['LargestPropertyUseType OSE'], building['sq_ft_classification'])
+        second_use_ghgi = self._find_ghgi_standard(year, orig_building_row['SecondLargestPropertyUseType OSE'], building['sq_ft_classification'])
+        third_use_ghgi = self._find_ghgi_standard(year, orig_building_row['ThirdLargestPropertyUseType OSE'], building['sq_ft_classification'])
 
         return (orig_building_row['LargestPropertyUseType Percent GFA'] * (baseline_ghgi if pd.isna(first_use_ghgi) else first_use_ghgi)) + (orig_building_row['SecondLargestPropertyUseType Percent GFA'] * (baseline_ghgi if pd.isna(second_use_ghgi) else second_use_ghgi)) + (orig_building_row['ThirdLargestPropertyUseType Percent GFA'] * (baseline_ghgi if pd.isna(third_use_ghgi) else third_use_ghgi))
 
@@ -123,17 +116,14 @@ class BaselineBEPSModel:
         else:
             return 0
 
-    def _calculate_baseline_model_without_saving(self, start_year, end_year, alternate_timeline):
+    def _calculate_baseline_model_without_saving(self, start_year, end_year):
         self._load_input_data()
-
-        if alternate_timeline:
-            alt_timeline = pd.read_csv(alternate_timeline)
         
         baseline_building_info = pd.DataFrame(columns=['OSEBuildingID', 'BuildingName', 'Total GFA for Policy', 'sq_ft_classification'])
         scen_calcs = self._fill_in_expected_baselines(start_year, end_year, self.building_data, baseline_building_info)
         
         scen_calcs['expected_baseline_ghgi'] = scen_calcs.apply(lambda building: self._get_expected_baseline_ghgi(building), axis=1)
-        scen_calcs['city_ghgi_target'] = scen_calcs.apply(lambda building: self._get_city_ghgi(building, alt_timeline), axis=1)
+        scen_calcs['city_ghgi_target'] = scen_calcs.apply(lambda building: self._get_city_ghgi(building), axis=1)
         scen_calcs['compliant_ghgi'] = scen_calcs.apply(lambda building: self._get_compliant_ghgi(building), axis=1)
         scen_calcs['compliant_emissions'] = scen_calcs.apply(lambda building: self._get_compliant_emissions(building), axis=1)
         scen_calcs['compliance_status'] = scen_calcs.apply(lambda building: self._get_compliance_status(building), axis=1)
@@ -141,13 +131,12 @@ class BaselineBEPSModel:
 
         return scen_calcs
     
-    def calculate_baseline_model(self, start_year, end_year, alternate_timeline = None):
+    def calculate_baseline_model(self, start_year, end_year):
         '''
             start_year: year to begin calculations (inclusive)
             end_year: year to end calculations (inclusive)
-            alternate_timeline: file path to an alternative timeline. Defaults to None
         '''
-        scen_calcs = self._calculate_baseline_model_without_saving(self, start_year, end_year, alternate_timeline)
+        scen_calcs = self._calculate_baseline_model_without_saving(start_year, end_year)
         self.scenario_results = scen_calcs
 
         print('Model calculations complete. Access the model dataframe as model_name.scenario_results')
@@ -172,50 +161,3 @@ class BaselineBEPSModel:
         emissions_in_target_year = self.scenario_results[self.scenario_results['year'] == year]['compliant_emissions'].sum()
 
         return 1 - (emissions_in_target_year / baseline_2026)
-
-    # Calculating how much to reduce targets in order to hit X% reduction in emissions
-    
-    def create_temp_emissions_timeline_file(percent):
-        # method to create temporary file for emissions
-        # percent is % of original baseline, not percent reduction of original baseline
-        
-        temp_timeline = self.timeline['ghgi'] * (percent / 100.0)
-        file_name = f'tmp/timeline_{percent}_percent_of_policy.csv'
-        temp_timeline.to_csv(file_name)
-        return temp_timeline, file_name
-
-    def find_reduction_percent(self, target_kg, target_year, file_path = None):
-        # Basic BST algorithm to find target reduction needed to hit a particular goal
-        
-        # make temporary directory for timeline files
-        os.mkdir('tmp')
-    
-        min_emissions = target_kg - 5000
-        max_emissions = target_kg + 5000
-    
-        high = 100
-        low = 0
-        found_target = False
-    
-        while not found_target:
-            percent_of_orig_emissions = (high + low) / 2
-            reduced_timeline, reduced_timeline_path = create_temp_emissions_timeline_file(percent_of_orig_emissions, timeline_path)
-
-            model = this._calculate_baseline_model_without_saving(target_year, target_year, reduced_timeline)
-            emissions_in_target_year = model['compliant_emissions'].sum()
-    
-            if min_emissions < emissions_in_target_year < max_emissions:
-                print(f'You can achieve {target_kg} kg/yr in {target_year} by reducing the baselines by {100 - percent_of_orig_emissions}%.')
-                # move file
-                os.rename(reduced_timeline_path, reduced_timeline_path.replace('tmp/', file_path))
-    
-                # delete tmp directory
-                shutil.rmtree('tmp')
-    
-                found_target = True
-    
-            else:
-                if emissions_in_2040 < target_kg:
-                    low = percent_of_orig_emissions
-                else:
-                    high = percent_of_orig_emissions
